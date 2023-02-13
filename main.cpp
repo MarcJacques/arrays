406339b927a776cb18724ddb5bee413d66e60ccd
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-02-13 22:13:02 +0000

    add for loop to print all data

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  const int SIZE = 5;
+
+  int list[SIZE];
+  list[0] = 10;
+  list[2] = 20;
+  list[4] = list[0] + list [2];
+
+  cout << "Enter array data";
+  cin >> list[1];
+  cin >> list[3];
+
+  cout << list[1] << endl;
+  int value;
+
+  for (int i = 0; i <= (sizeof(list) - 1); i++)
+    {
+      value = list[i];
+      cout << value << endl;
+    }
+  return 0;
 }
